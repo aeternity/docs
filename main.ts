f7007e421b999f8908052b100f63b56eb02d83dc
Yusuf Seyrek <seyrek91@gmail.com>
2025-02-04 17:32:50 +0300

    feat: add function to format GitHub URLs in markdown files

--- main.ts
+++ main.ts
@@ -71,7 +71,31 @@ function findMarkdownFilesAndCopyToDocs(dir: string) {
 
     createFolderIfNotExists(destFolder);
     fs.copyFileSync(file, destFile);
+    formatFileContent(destFile);
   });
+}
+
+function formatFileContent(file: string) {
+  let fileChanged = false;
+  let content = fs.readFileSync(file, "utf8");
+  const ghBlobUrlRegex =
+    /https:\/\/github\.com\/[A-Za-z]+\/([A-Za-z]+(-[A-Za-z]+)+)\/blob\/[A-Za-z0-9]+\/([A-Za-z]+(\/[A-Za-z]+)+)\.[A-Za-z]+#L[0-9]+-L[0-9]+/g;
+
+  for (const match of content.matchAll(ghBlobUrlRegex)) {
+    const url = match[0];
+    const codeBlockForUrl = `{% @github-files/github-code-block url="${url}" %}`;
+
+    if (
+      content.includes(codeBlockForUrl) ||
+      content[content.indexOf(url) - 1] !== "\n" // checking if the url is used in a link
+    )
+      continue;
+
+    content = content.replace(url, codeBlockForUrl);
+    fileChanged = true;
+  }
+
+  if (fileChanged) fs.writeFileSync(file, content);
 }
 
 // JOB FUNCTION
